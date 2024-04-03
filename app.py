ee2a6c7b912f5869e9efc7ff6060c2ef4a0ee930
yeham <yeham@onelineai.com>
2024-04-03 22:38:40 +0900

    주식 검색 및 search stock 기능 추가

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 from langchain_core.callbacks import BaseCallbackHandler
 
-from src.chain.conversation import sample_conversation_chain
+from src.chain.conversation import sample_conversation_chain, search_stock, search_stock_verified
 
 
 class OpenAIChatMessageCallbackHandler(BaseCallbackHandler):
@@ -43,11 +43,34 @@ def streamlit_init():
 
     if "messages" not in st.session_state:
         st.session_state["messages"] = []
+        st.session_state["background"] = """
+삼성전자 (005930.KS) - 삼성전자는 프로메테우스 프로젝트의 개발을 주도하며, 자사의 고성능 GPU와 NPU를 통해 AI 분야에서 기술력을 입증했습니다. 2030년 현재, 삼성전자는 프로메테우스를 스마트폰, 스마트 TV 등 소비자 가전 제품에 탑재해 차별화된 사용자 경험을 제공하고 있습니다. 또한, 산업용 AI 솔루션 개발에도 나서며 스마트 팩토리와 스마트 시티 분야에서 혁신을 주도하고 있습니다.
+
+SK하이닉스 (000660.KS) - SK하이닉스는 AI 옵티마이저 프로젝트를 통해 독자적인 AGI 기술 개발에 집중하며, 메모리 반도체 설계와 AI 알고리즘을 결합하여 생산 효율성을 높이고 범용 AGI 기술 발전에 기여하고 있습니다. 현재는 프로메테우스의 발전을 지켜보며 상호 협력 가능성을 모색하고 있으며, AI 옵티마이저를 프로메테우스 수준으로 고도화하는 도전에는 시간과 노력이 필요할 것으로 예상됩니다.
+
+네이버 (035420.KS) - 네이버의 AGI 모델 '하이퍼클로바 X'개발하여 프로메테우스와 경쟁 중이며, 성능 면에서는 아직 미흡하지만 방대한 데이터와 도메인 전문성으로 경쟁력을 갖추고 있습니다. 하이퍼클로바 X를 자사 서비스에 접목하여 혁신적인 사용자 경험을 제공하고 동시에 B2B 시장 공략을 가속화하며 글로벌 시장에진출하기 위한 계획을 세우고 있습니다.
+
+카카오 (035720.KS) - 카카오의 AI 모델 '카카오브레인'은 현재 네이버나 프로메테우스에 비해 성능이 미흡하며, 제한된 도메인에서의 서비스만을 제공하고 있으며 AGI로의 발전 가능성은 낮아 보입니다. 카카오는 AI 기술 개발이 상대적으로 늦어진 점과 투자 규모의 한정, 인재 확보 어려움 등으로 경쟁에서 뒤처진 상황이며, 이를 극복하기 위해 공격적인 AI 투자와 인수합병을 통해 기술력과 인재 풀을 강화하고 있습니다.
+
+LG전자 (066570.KS) - LG전자는 프로메테우스를 가전제품과 산업용 솔루션에 접목하여 세계 시장을 선도하고 있으며, 국내에서 프로메테우스의 최대 고객으로 혁신적 제품과 서비스를 출시하고 있습니다. 프로메테우스를 탑재한 엘지의 스마트 가전 제품은 사용자 행동 패턴을 학습하여 최적화된 성능과 직관적 인터페이스를 제공하며, 파트너십은 업계에서 윈-윈 전략의 모범 사례로 평가되고 있습니다.
+
+셀바스AI (108860.KS) - 셀바스AI는 HCI 기술로 디지털 전환을 주도했지만, 프로메테우스의 등장으로 시장에서 타격을 입었고 경쟁력을 유지하기 위해 AGI 기술에 투자했으나 대형 파트너들의 전략적 파트너십으로 뒤처지기 시작했습니다. AGI의 범용성과 유연성에 비해 셀바스AI의 기술은 제한적이어서 시장에서의 점유율을 잃고 있습니다.
+
+한글과컴퓨터 (030520.KR) - 한글과컴퓨터는 프로메테우스 AGI의 부상으로 오피스 소프트웨어 시장에서의 변화에 대응하기 위해 전략을 재조정했습니다. 기존 역량과 공공기관 및 기업 시장에서의 입지를 활용하며, 오픈 소스 커뮤니티와 협력하여 AI 기능을 플러그인으로 개발하고 '한컴구름' OS를 AGI와 연동하는 개방형 플랫폼으로 전환하고자 노력하고 있습니다.
+
+셀트리온 (068270.KR) - 셀트리온은 프로메테우스 AGI 기술의 도입에 대해 신중한 입장을 보이며, 의료 데이터의 민감성과 윤리적 문제를 우려하고 있습니다. 독점적 사용으로 인한 기술 격차와 접근성 문제에 대한 우려도 표명하며, 자체 연구 개발과 협력을 통해 의약품 개발에 필요한 기술을 확보하는 전략을 채택하여 환자 중심의 의료 서비스 제공을 최우선으로 삼고 있습니다.
+
+크래프톤 (259960.KR) - 크래프톤은 삼성전자와의 협약을 통해 프로메테우스 AGI 기술을 게임 개발에 통합하여 혁신적인 변화를 이끌고 있습니다. 게임 내 인공지능을 게임의 모든 측면에 적용하여 음성, 비전, 언어 처리 등 다양한 분야에서 AGI 기술을 활용하고 있으며, 특히 '버추얼 프렌드' 개발에 집중하여 플레이어에게 개인화된 경험을 제공하고 있습니다. 
+
+LS ELECTRIC (010120.KR) - LS ELECTRIC은 프로메테우스 AGI 기술을 활용한 '스마트 에너지 솔루션'을 개발하여, 전력과 자동화 분야에서 혁신적인 도약을 이루고 있습니다. 전력 부문에서는 지능형 전력 관리 시스템을 통해 효율성과 안정성을 극대화하며, 자동화 부문에서는 AGI 기반 자동화 제어 시스템을 통해 생산성과 에너지 사용을 최적화하고 고장 예측 및 유지보수를 자동화합니다.
+        """
         st.session_state["market"] = []  # 시장
         st.session_state["stocks"] = []  # 시장
-        st.session_state["portfolio"] = []  # 유저 포트폴리오
+        # st.session_state["portfolio"] = []  # 유저 포트폴리오
         st.session_state["actions"] = []  # 유저 액션
         st.session_state["time"] = 0  # 시간
+        st.session_state["stock_info"] = 0  # 유저 포트폴리오 정보
+        st.session_state["stock_info_df"] = 0  # 유저 포트폴리오 정보
 
     with col1:
         with st.container(height=600):
@@ -64,7 +87,7 @@ def streamlit_init():
 5. 2~4단계를 게임 내 시간으로 10년이 될 때까지 반복하세요.
 6. 10년 동안 여러분의 포트폴리오 가치를 최대한 높이는 것이 목표입니다. 현명한 투자 결정과 리스크 관리가 성공의 열쇠가 될 것입니다.
 
-그럼 이제 게임을 시작하겠습니다. 검색하고 싶은 주식을 설명해주세요. 매턴 주식 조사는 한번만 가능하니 신중하게 해야합니다. (ex. PROMETHEUS 의 경쟁사를 개발 중인 회사는 누가 있어?, PROMETHEUS 개발 참여 기업, 등)"""
+그럼 이제 게임을 시작하겠습니다. 검색하고 싶은 주식을 설명해주세요. 매턴 주식 조사는 한번만 가능하니 신중하게 해야합니다. (ex. PROMETHEUS 의 경쟁사를 개발 중인 회사는 뭐가 있어?, PROMETHEUS 개발 참여 기업, 등)"""
                     ,
                     "ai",
                 )
@@ -73,13 +96,25 @@ def streamlit_init():
 
             if message := st.chat_input(""):
                 save_message(message, "human")
+                print(message)
+                # edit_df = st.data_editor(df)
+
+                st.session_state['stock_info'] = st.session_state["stock_info_df"].to_dict('records')
+                ratio_sum = st.session_state["stock_info_df"]['ratio'].sum()
+                print(st.session_state['stock_info'], ratio_sum)
 
                 with st.chat_message("human"):
                     st.markdown(message)
 
                 with st.chat_message("ai"):
-                    save_message(
-                        sample_conversation_chain(callbacks=[OpenAIChatMessageCallbackHandler()]).invoke(message), "ai")
+                    return_value = search_stock_verified(message)
+                    print("search_stock_verified 결과:", return_value)
+                    if return_value.content == '[YES]':
+                        save_message(
+                            search_stock(inputs=message, background=st.session_state["background"], callbacks=[OpenAIChatMessageCallbackHandler()]), "ai"
+                        )
+                    # if ratio_sum != 100:
+                    #     save_message("비율의 합이 100이 되어야합니다.", "ai")
 
     with col2:
         with st.container(height=300):
@@ -98,14 +133,15 @@ def streamlit_init():
                     {"command": "카카오", "ratio": 3, "price": 300000},
                 ]
             )
-            edited_df = st.data_editor(df)
+            st.session_state["stock_info_df"] = st.data_editor(df)
+            # print(edited_df.to_dict('records'))
 
     # NOTE: 사용자 액션이 일어나는 것을 캐치 -> 변화가 있으면 json에 저장하기
     # NOTE: 수정 버튼 입력해서 그 버튼을 눌렀을 경우에만 해당 데이터를 파싱하는 방법으로 받아오기
 
-    print(option)
-    dict_output = df.to_dict('records')
-    print(dict_output)
+    # print(option)
+    # dict_output = df.to_dict('records')
+    # print(dict_output)
 
 if __name__ == "__main__":
     st.set_page_config(
--- src/chain/conversation.py
+++ src/chain/conversation.py
@@ -15,7 +15,7 @@ def sample_conversation_chain(callbacks: List[BaseCallbackHandler] = [], ):
     return {"inputs": RunnablePassthrough()} | prompt | llm
 
 
-def search_stock(inputs: str, background: str):
+def search_stock(inputs: str, background: str, callbacks: List[BaseCallbackHandler] = []):
     prompt = PromptTemplate.from_template("""
 # Persona
 You are fictional story simulator. Today is 2030 and an AGI called prometheus has been developed. Given the background answer the user's questions. You may add with your imagination. However, you may not introduce new companies other then the ones provided. Always answer in formal Korean. 
@@ -26,11 +26,16 @@ You are fictional story simulator. Today is 2030 and an AGI called prometheus ha
 ### User Question: {inputs}
 ### Response:
 """)
-    llm = get_azure_gpt_chat_llm(model_version="4")
+    llm = get_azure_gpt_chat_llm(model_version="4", is_stream=True, callbacks=callbacks)
     chain = {"inputs": RunnablePassthrough(), "background": RunnablePassthrough()} | prompt | llm
     response = chain.invoke({"inputs": inputs, "background": background})
-    print(response.__dict__)
-    return response
+    return response.content
+
+    # chain = {"inputs": RunnablePassthrough(), "background": RunnablePassthrough()} | prompt | llm
+    # chain = {"inputs": RunnablePassthrough(), "examples": RunnablePassthrough()} | prompt | llm
+    # response = chain.invoke({"inputs": inputs, "background": background})
+    # print(response.__dict__)
+    # return response
 
 def search_stock_verified(inputs: str):
     """
@@ -38,53 +43,67 @@ def search_stock_verified(inputs: str):
     :param inputs:
     :return:
     """
+#     prompt = PromptTemplate.from_template("""
+# # Persona
+# You're the best linguistic analyst.
+# You're the best at understanding and analysing the intent of a user's speech.
+#
+#
+# ## Environment
+# Mostly, users will ask questions in Korean, and they will ask questions related to stocks or the stock market and the economy.
+#
+#
+# ## Condition
+# 1) Whatever question the user is asking, you need to summarise the core of their intent in as short and concise a way as possible, no more than 3 sentences.
+# 2) You need to suggest nouns or verbs that we can extract as keywords.
+# 3) If you have information about the user, be sure to add it to your answer.
+# 4) **Answer no to all unless this is a search for stocks**
+#
+#
+# ## Important Rules
+# The final answer should be in JSON format, preferably in Korean, but if not, you can use English.
+# No other language is required.
+#
+#
+# ## Examples
+# {examples}
+#
+#
+# ## User Question: {inputs}
+#
+#
+# Your Answer:
+# """)
     prompt = PromptTemplate.from_template("""
-# Persona
-You're the best linguistic analyst.
-You're the best at understanding and analysing the intent of a user's speech.
+    # Persona
+    You're the best linguistic analyst.
+    You will be given a question verify whether the question requires you to list stocks that fits a certain criteria.
 
 
-## Environment
-Mostly, users will ask questions in Korean, and they will ask questions related to stocks or the stock market and the economy.
+    ## Environment
+    Mostly, users will ask questions in Korean, and they will ask questions related to stocks or the stock market and the economy.
 
+    ## Important Rules
+    If the you think the question is valid asking for a list of stocks return [YES] if not return [NO].
 
-## Condition
-1) Whatever question the user is asking, you need to summarise the core of their intent in as short and concise a way as possible, no more than 3 sentences.
-2) You need to suggest nouns or verbs that we can extract as keywords.
-3) If you have information about the user, be sure to add it to your answer.
-4) **Answer no to all unless this is a search for stocks**
-
-
-## Important Rules
-The final answer should be in JSON format, preferably in Korean, but if not, you can use English. 
-No other language is required.
-
-
-## Examples
-{examples}
-
-
-## User Question: {inputs}
-
-
-Your Answer:
-""")
+    ## User Question: {inputs}
+    Your Answer:""")
     examples = [
         {
             "question": "삼성전자 에 대해 설명해줘",
-            "answer": '{"Intent" : "종목 설명", "Keywords" : ["삼성전자", "설명"]}'
+            "answer": '[NO]'
         },
         {
-            "question": "현재 대한민국 주식 시장에 대해 설명해줘",
-            "answer": '{"Intent" : "대한민국 주식 시장 설명", "Keywords" : ["대한민국", "주식 시장", "설명"]}'
+            "question": "반도체를 다루는 회사들을 소개해줘",
+            "answer": '[YES]'
         },
         {
-            "question": "AGI 의 등장이 인간에게 미치는 영향은 어떻게 되고, 그에 따라 주식 시장은 어떤 종목들이 상승하고 하락을 하게 될까?",
-            "answer": '{"Intent" : "AGI(Artificial General Intelligence) 등장에 따른 인간 사회, 주식 시장 영향", "Keywords" : [None]}'
+            "question": "AGI 의 등장에 영향을 받는 회사들은 뭐가 있어?",
+            "answer": '[YES]'
         },
         {
             "question": "인간의 종말이 온다면, 그것은 나에게 어떤 영향을 줄것인가?",
-            "answer": '{"Intent" : "I`m not sure about this.", "Keywords" : [None]}'
+            "answer": '[NO]'
         },
     ]
     llm = get_azure_gpt_chat_llm(model_version="4", is_stream=True)
